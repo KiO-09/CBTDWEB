a05ec939443d7a18133fcefb9f37074d490c3e17
caio <cavians2015@gmail.com>
2025-04-14 20:28:08 -0300

    teste commit

---
+++ Nova pasta/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string? nome;
+Console.WriteLine("Digite seu nome: ");
+nome = Console.ReadLine();
+
+Console.WriteLine("Olá " + nome + "!");
+Console.ReadLine();
+
+int n2=0;
+float media=0;
+int n1=0;
+Console.WriteLine ("digite uma nota");
+n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("digite outra nota");
+n2 = Convert.ToInt32(Console.ReadLine());
+media = (n1 + n2)/2;
+Console.WriteLine ("sua média é: " + media);
+
+Console.ReadLine();
